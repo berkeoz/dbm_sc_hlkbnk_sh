d2c0b0cccf5762416bf9af987d1e4465d5ce825c
berkeoz <berkeoz>
2024-05-01 09:12:05 -0700

    Table 1 column Change ; TaskID: 1

--- dbo/Tables/Creation-Scripts/Table_1.sql
+++ dbo/Tables/Creation-Scripts/Table_1.sql
@@ -4,6 +4,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Table_1](
 	[col1] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[col2] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
  CONSTRAINT [PK_Table_1] PRIMARY KEY CLUSTERED 
 (
 	[col1] ASC
